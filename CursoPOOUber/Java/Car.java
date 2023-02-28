00d2740d05e3a62f39dec1a609ca19e46f73d599
IvanRomero-io <ivandejesusrl2001@gmail.com>
2023-02-28 10:01:56 -0500

    objetos con java

--- CursoPOOUber/Java/Car.java
+++ CursoPOOUber/Java/Car.java
@@ -1,8 +1,12 @@
 package CursoPOOUber.Java;
 
 public class Car {
-    Integer id;
-    String license;
-    String driver;
-    Integer passenger;
+    Integer id;        //atributo
+    String license;    //atributo
+    String driver;     //atributo
+    Integer passenger; //atributo
+
+    void printDataCar(){
+        System.out.println("License: " + license + " Driver: " + driver); //aca creamos una funcion que va a ser llamada como metodo, nos ahorra tener que escribir esta misma funcion y se crear al hacer el objeto a partir de la clase
+    }
 }
