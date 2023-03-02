0bdef0116e3f805ca75feeebf2323fee4bee874b
IvanRomero-io <ivandejesusrl2001@gmail.com>
2023-03-02 12:11:40 -0500

    objetos en python

--- CursoPOOUber/Python/main.py
+++ CursoPOOUber/Python/main.py
@@ -1,13 +1,20 @@
 from car import Car
+from account import Account
+
 
 if __name__ == "__main__":
     print("Hola mundo")
-    car = Car()
-    car.license = "AMS234"
-    car.driver  = "Andres Herrera" 
-    print(vars(car))
 
-    car2 = Car()
-    car2.license = "AFP865"
-    car2.driver  = "Claudio Bravo"
-    print(vars(car2))
+    car = Car("AMS234", Account("Ivan Romero", "ANDA876"))
+    print(vars(car))
+    print(vars(car.driver))
+
+    # car = Car()
+    # car.license = "AMS234"
+    # car.driver  = "Andres Herrera" 
+    # print(vars(car))
+
+    # car2 = Car()
+    # car2.license = "AFP865"
+    # car2.driver  = "Claudio Bravo"
+    # print(vars(car2))
