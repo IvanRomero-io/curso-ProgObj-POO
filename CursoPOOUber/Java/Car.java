c8496442d0d890d43d0c7139f398154220305ee7
IvanRomero-io <ivandejesusrl2001@gmail.com>
2023-03-03 12:33:59 -0500

    Herencia en java

--- CursoPOOUber/Java/Car.java
+++ CursoPOOUber/Java/Car.java
@@ -3,8 +3,13 @@ package CursoPOOUber.Java;
 public class Car {
     Integer id;        //atributo
     String license;    //atributo
-    String driver;     //atributo
+    Account driver;     //atributo
     Integer passenger; //atributo
+
+    public Car(String license, Account driver){
+        this.license  = license;
+        this.driver   = driver;
+    }
 
     void printDataCar(){
         System.out.println("License: " + license + " Driver: " + driver); //aca creamos una funcion que va a ser llamada como metodo, nos ahorra tener que escribir esta misma funcion y se crear al hacer el objeto a partir de la clase
--- CursoPOOUber/Java/Main.java
+++ CursoPOOUber/Java/Main.java
@@ -1,19 +1,19 @@
-package CursoPOOUber.Java;
+// package CursoPOOUber.Java;
 
-public class Main {
-     public static void main(String[] args){
-        System.out.println("Hola Mundo");
-        Car car = new Car();
-        car.license = "CAD027";
-        car.driver = "Ivan";
-        car.passenger = 4;
-        car.printDataCar();
+// public class Main {
+//      public static void main(String[] args){
+//         System.out.println("Hola Mundo");
+//         Car car = new Car();
+//         car.license = "CAD027";
+//         car.driver = "Ivan";
+//         car.passenger = 4;
+//         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "AFP856";
-        car2.driver = "Andrea Herrera";
-        car2.passenger = 3;
-        car2.printDataCar();
-     }
-}
+//         Car car2 = new Car();
+//         car2.license = "AFP856";
+//         car2.driver = "Andrea Herrera";
+//         car2.passenger = 3;
+//         car2.printDataCar();
+//      }
+// }
 
