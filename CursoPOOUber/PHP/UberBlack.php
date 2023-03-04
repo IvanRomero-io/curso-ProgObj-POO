f2fe6357e4daceca40d9ef520362ef22ffb04d79
IvanRomero-io <ivandejesusrl2001@gmail.com>
2023-03-04 08:36:10 -0500

    objetos php parte 1

---
+++ CursoPOOUber/PHP/UberBlack.php
@@ -0,0 +1,12 @@
+<?php
+
+class UberBlack extends Car{
+    public $typeCarAccepted;
+    public $seatsMaterial;
+
+    public function __construct($license, $driver, $brand, $model){
+        parent::__construct($license, $driver)
+        $this->typeCarAccepted = $typeCarAccepted
+        $this->seatsMaterial = $seatsMaterial 
+    }
+}
--- CursoPOOUber/PHP/index.php
+++ CursoPOOUber/PHP/index.php
@@ -1,8 +1,17 @@
 <?php
 
-require_once('car.php')
-require_once('account.php')
+// require_once('car.php')
+// require_once('account.php')
 
-$car = new Car("AW456", new Account("Andres Herrera", "AMS123"))
+// $car = new Car("AW456", new Account("Andres Herrera", "AMS123"))
 
-$car->PrintDataCar()
+// $car->PrintDataCar()
+
+require_once("car.php");
+require_once("uberX.php");
+require_once("account.php");
+
+$uberx = new UberX("CVB123", new Account("Andres Herrera", "AND456"), "Chevrolet", "Spark");
+$uberX->printDataCar();
+
+?>
